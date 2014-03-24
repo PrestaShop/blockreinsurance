6f6435700a0b0ef06a5e84fb50413cff1384b170
Quetzacoalt91 <thomas.nabord@prestashop.com>
2014-03-24 11:43:28 +0100

    / MO blockreinsurance : ps_versions_compliancy modified (1.5.6.1 => 1.6)

--- blockreinsurance.php
+++ blockreinsurance.php
@@ -47,7 +47,7 @@ class Blockreinsurance extends Module
 
 		$this->displayName = $this->l('Customer reassurance block');
 		$this->description = $this->l('Adds an information block aimed at offering helpful information to reassure customers that your store is trustworthy.');
-		$this->ps_versions_compliancy = array('min' => '1.5.6.1', 'max' => _PS_VERSION_);
+		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
 	}
 
 	public function install()
